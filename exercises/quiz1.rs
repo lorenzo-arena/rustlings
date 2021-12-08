74bb95cc0a92b06136cc029c26c0b2ab02b510ff
Lorenzo Arena <arena.lor@gmail.com>
2021-12-08 20:36:03 +0100

    quiz1: add implementation

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,16 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
+const QUANTITY_THRESHOLD: u32 = 40;
 
 // Put your function here!
-// fn calculate_apple_price {
+fn calculate_apple_price(quantity: u32) -> u32 {
+    if quantity > QUANTITY_THRESHOLD {
+        quantity
+    } else {
+        2 * quantity
+    }
+}
 
 // Don't modify this function!
 #[test]
